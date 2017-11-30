dc636ddfe0ae7921e3fc679939735d105959be91
Kusum Dhoundiyal <kusum.dhoundiyal96@gmail.com>
2017-11-30 17:00:32 +0530

    setonline function changes

--- src/js/app/fullview/applozic.fullview.js
+++ src/js/app/fullview/applozic.fullview.js
@@ -184,7 +184,7 @@ var MCK_CLIENT_GROUP_MAP = [];
                         return 'success';
                         break;
                     case 'setOnline':
-                        oInstance.setOffline();
+                        oInstance.setOnline();
                         return 'success';
                         break;
                    case 'reset':
